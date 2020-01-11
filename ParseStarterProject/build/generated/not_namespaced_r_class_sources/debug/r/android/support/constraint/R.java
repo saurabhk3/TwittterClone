be557251cc34dff123b5b075ecf69a451e8005d0
saurabhk3 <kannaujia944@gmail.com>
2020-01-11 18:32:28 +0530

    Initial Commit

--- ParseStarterProject/build/generated/not_namespaced_r_class_sources/debug/r/android/support/constraint/R.java
+++ ParseStarterProject/build/generated/not_namespaced_r_class_sources/debug/r/android/support/constraint/R.java
@@ -73,20 +73,20 @@ public final class R {
     public static final class id {
         private id() {}
 
-        public static final int bottom = 0x7f080022;
-        public static final int end = 0x7f080059;
-        public static final int gone = 0x7f08005e;
-        public static final int invisible = 0x7f080079;
-        public static final int left = 0x7f08007b;
-        public static final int packed = 0x7f080093;
-        public static final int parent = 0x7f080094;
-        public static final int percent = 0x7f080097;
-        public static final int right = 0x7f0800a8;
-        public static final int spread = 0x7f0800c6;
-        public static final int spread_inside = 0x7f0800c7;
-        public static final int start = 0x7f0800cc;
-        public static final int top = 0x7f0800e8;
-        public static final int wrap = 0x7f0800f0;
+        public static final int bottom = 0x7f080023;
+        public static final int end = 0x7f08005a;
+        public static final int gone = 0x7f08005f;
+        public static final int invisible = 0x7f08007a;
+        public static final int left = 0x7f08007c;
+        public static final int packed = 0x7f080094;
+        public static final int parent = 0x7f080095;
+        public static final int percent = 0x7f080098;
+        public static final int right = 0x7f0800a9;
+        public static final int spread = 0x7f0800c7;
+        public static final int spread_inside = 0x7f0800c8;
+        public static final int start = 0x7f0800cd;
+        public static final int top = 0x7f0800e9;
+        public static final int wrap = 0x7f0800f1;
     }
     public static final class styleable {
         private styleable() {}
--- ParseStarterProject/src/main/java/com/parse/starter/MainActivity.java
+++ ParseStarterProject/src/main/java/com/parse/starter/MainActivity.java
@@ -8,12 +8,18 @@
  */
 package com.parse.starter;
 import android.content.Intent;
+import android.media.Image;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.view.KeyEvent;
 import android.view.View;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.RelativeLayout;
 import android.widget.Switch;
+import android.widget.Toast;
 
 import com.parse.LogInCallback;
 import com.parse.Parse;
@@ -22,11 +28,29 @@ import com.parse.ParseAnonymousUtils;
 import com.parse.ParseException;
 import com.parse.ParseUser;
 import com.parse.SaveCallback;
+import com.parse.SignUpCallback;
 
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener,View.OnKeyListener{
 
   EditText username ,password;
+
+  @Override
+  public boolean onKey(View view, int i, KeyEvent keyEvent) {
+    if(i == KeyEvent.KEYCODE_ENTER && keyEvent.getAction()==KeyEvent.ACTION_DOWN){
+      submit(view);
+    }
+    return false;
+  }
+
+  @Override
+  public void onClick(View view) {
+    if(view.getId()==R.id.imageView || view.getId()==R.id.background){
+      Log.i("Imge","Clicked");
+      InputMethodManager imm = (InputMethodManager)getSystemService(INPUT_METHOD_SERVICE);
+      imm.hideSoftInputFromWindow(getCurrentFocus().getWindowToken(),0);
+    }
+  }
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
@@ -35,6 +59,13 @@ public class MainActivity extends AppCompatActivity {
 
     username = (EditText) findViewById(R.id.username);
     password = (EditText) findViewById(R.id.password);
+    RelativeLayout layout = (RelativeLayout) findViewById(R.id.background);
+    ImageView logo = (ImageView) findViewById(R.id.imageView);
+    username.setOnKeyListener(this);
+    password.setOnKeyListener(this);
+    layout.setOnClickListener(this);
+    logo.setOnClickListener(this);
+
 
     
     ParseAnalytics.trackAppOpenedInBackground(getIntent());
@@ -42,6 +73,24 @@ public class MainActivity extends AppCompatActivity {
   public void submit(View view){
     Log.i("username:",username.getText().toString());
     Log.i("pass:",password.getText().toString());
+
+    String user = username.getText().toString();
+    String pass = password.getText().toString();
+
+    ParseUser parseUser = new ParseUser();
+    parseUser.setUsername(user);
+    parseUser.setPassword(pass);
+
+    parseUser.signUpInBackground(new SignUpCallback() {
+      @Override
+      public void done(ParseException e) {
+        if(e==null){
+          Toast.makeText(MainActivity.this, "Successfully signed up", Toast.LENGTH_SHORT).show();
+        }else{
+          e.printStackTrace();
+        }
+      }
+    });
 
   }
 
